25bcd86d4f897f99ea254861f6ab1b0d694e576e
Marivaldo Sena <msena.ifsp@gmail.com>
2019-04-14 02:43:09 -0300

    Implementar endpoint para a criação de usuário.

--- app/routes/users_routes.py
+++ app/routes/users_routes.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, jsonify
-from app.models import Usuario
+from flask import Blueprint, jsonify, request
+from app.models import Usuario, db
 
 users_bp = Blueprint('users', __name__)
 
 
 @users_bp.route('/')
 def get_all_users():
-    usuarios = Usuario.query.all()
+    usuarios = [usuario.to_json() for usuario in Usuario.query.all()]
 
     return jsonify({'usuarios': usuarios})
 
@@ -14,3 +14,19 @@ def get_all_users():
 @users_bp.route('/<int:user_id>')
 def get_user(user_id):
     return '{}'.format(user_id)
+
+
+@users_bp.route('/', methods=['POST'])
+def create_user():
+    json = request.get_json()
+
+    usuario = Usuario(nome=json.get('nome', None),
+                      email=json.get('email', None),
+                      senha=json.get('senha', None)
+                      )
+
+    db.session.add(usuario)
+    db.session.commit()
+
+    return jsonify(usuario.to_json()), 201
+
--- app/tests/views/test_views.py
+++ app/tests/views/test_views.py
@@ -3,14 +3,35 @@ import json
 
 
 class TestView:
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
     def test_get_all_users(self, client):
         response = client.get(url_for('users.get_all_users'))
-        dict = json.loads(response.data)
+        dados = json.loads(response.data)
         assert response.status_code == 200
-        assert isinstance((dict['usuarios']), list)
+        assert isinstance(dados['usuarios'], list)
 
     def test_get_single_user(self, client):
         user_id = 1
         response = client.get(url_for('users.get_user', user_id=user_id))
         assert response.status_code == 200
         assert str(user_id) in str(response.data)
+
+    def test_create_user(self, client):
+        dados = {
+            'nome': 'nome1',
+            'email': 'email1',
+            'senha': 'senha1'
+        }
+
+        response = client.post(url_for('users.create_user'),
+                               headers=self.headers,
+                               data=json.dumps(dados))
+        json_data = json.loads(response.data)
+
+        assert response.status_code == 201
+        assert json_data['nome'] == dados['nome']
+        assert json_data['email'] == dados['email']
+        assert json_data.get('senha') is None
